b1edcc83c47130f81fdbc726f4d8e5d3bf0aa8b3
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2023-03-28 08:12:17 +0200

    Added Function to seed Tensorflow
    source: https://wandb.ai/sauravmaheshkar/RSNA-MICCAI/reports/How-to-Set-Random-Seeds-in-PyTorch-and-Tensorflow--VmlldzoxMDA2MDQy

--- lessons/lesson_8_code.py
+++ lessons/lesson_8_code.py
@@ -8,7 +8,18 @@ import gymnasium, collections
 
 # Setting the seeds
 SEED = 15
-np.random.seed(SEED); tf.random.set_seed(SEED)
+
+def set_seed(seed: int = 42) -> None:
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    tf.experimental.numpy.random.seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f"Random seed set as {seed}")
 
 
 def createDNN( nInputs, nOutputs, nLayer, nNodes ):
@@ -133,7 +144,9 @@ def main():
 	print( "*  Welcome to the eighth lesson of the RL-Lab!   *" )
 	print( "*               (Deep Q-Network)                 *" )
 	print( "**************************************************\n" )
-
+	
+	set_seed(SEED)
+	
 	_training_steps = 100
 
 	env = gymnasium.make( "CartPole-v1" )#, render_mode="human" )
